5e9bb493575abde0f6c14e88c23de041e107ad3d
Rushane Munganda <rushanemunganda@Rushanes-MacBook-Pro.local>
2024-07-30 13:12:29 +0530

    Final commit

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 let color = "black";
+let click = false;
+
 document.addEventListener("DOMContentLoaded", function(){
+    createBoard(32);
     console.log("Board is being created")
+
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON"){
+            click != click;
+            let draw = document.querySelector("#draw")
+            if(click){
+                draw.innerHTML = "Now you can Draw!!";
+            } else {
+                draw.innerHTML = "You are not allowed to Draw";
+            }
+        }
+    })
+
+
     let btn_popup = document.querySelector("#popup")
     btn_popup.addEventListener("click", function(){
         let size = getSize();
         createBoard(size);
     })
 
-    createBoard(32);
     // getSize();
 })
 
@@ -40,10 +56,10 @@ function getSize(){
 }
 
 function colorDiv(){
-    if(color == 'random'){
-        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+    if (color == "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
     } else {
-        this.style.backgroundColor = 'black';
+      this.style.backgroundColor = "black";
     }
 }
 
